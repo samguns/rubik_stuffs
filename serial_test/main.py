17787160689d402a808cfbc486793e6ce8000fba
samguns <wang.vai@gmail.com>
2014-12-07 23:33:12 +0800

    2014-12-7

--- serial_test/main.py
+++ serial_test/main.py
@@ -4,26 +4,26 @@ __author__ = 'sam'
 from serial import *
 
 ser = Serial()
-ser.port = 5
-ser.timeout = 30
+ser.port = 3
+ser.timeout = 300
 if False == ser.isOpen():
     ser.open()
 
-for i in range(0, 30):
-    recv_bytes = ser.read(14)
-    print("%d %d %d %d %d %d %d %d %d %d %d %d" % (int(recv_bytes[0].encode("hex"), 16),
-                            int(recv_bytes[1].encode("hex"), 16),
-                            int(recv_bytes[2].encode("hex"), 16),
-                            int(recv_bytes[3].encode("hex"), 16),
-                            int(recv_bytes[4].encode("hex"), 16),
-                            int(recv_bytes[5].encode("hex"), 16),
-                            int(recv_bytes[6].encode("hex"), 16),
-                            int(recv_bytes[7].encode("hex"), 16),
-                            int(recv_bytes[8].encode("hex"), 16),
-                            int(recv_bytes[9].encode("hex"), 16),
-                            int(recv_bytes[10].encode("hex"), 16),
-                            int(recv_bytes[11].encode("hex"), 16)))
+for i in range(0, 5):
+    text = ser.read(1)
+    if text:
+        n = ser.inWaiting()
+        if n:
+            text = text + ser.read(n)
 
-#s_data = '\x80\x09\x00\x06\x49\x00\x50\x00\x51\x00'
-#ser.write(s_data)
+        print("Received %d bytes" % len(text))
+
+        for recv_bytes in text:
+            print("%d  " % (int(recv_bytes.encode("hex"), 16)))
+
+    print "\r\r"
+
+
+#out_data = '\x0a\x00\x80\x09\x00\x06\x49\x00\x50\x00\x51\x00'
+#ser.write(out_data)
 ser.close()
--- serial_test/test.py
+++ serial_test/test.py
@@ -4,8 +4,14 @@ __author__ = 'sam'
 from serial import *
 import struct
 
-s_data = '\x80\x09\x00\x06\x49\x00\x50\x00\x51\x00'
-print("%d %d %d %d" % (int(s_data[0].encode("hex"), 16),
-						int(s_data[1].encode("hex"), 16),
-						int(s_data[2].encode("hex"), 16),
-						int(s_data[3].encode("hex"), 16)))
+ser = Serial()
+ser.port = 3
+ser.timeout = 300
+if False == ser.isOpen():
+    ser.open()
+
+for i in range(0, 50):
+    out_data = '\x0a\x00\x80\x09\x00\x06\x49\x00\x50\x00\x51\x00'
+    ser.write(out_data)
+
+ser.close()
